5aea38d672c3be1cc8e16209bd2020a9453254a8
Francis <Francis@DESKTOP-LSS51BN>
2022-06-12 22:13:13 +0300

    Add project files.

---
+++ PTSLibrary/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSLibrary
+{
+    internal class Customer : User
+    {
+        public Customer(string name, int id) 
+        {
+            this.name = name;
+            this.id = id;
+        }
+    }
+}
---
+++ PTSLibrary/TeamLeader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTSLibrary
+{
+    internal class TeamLeader : User
+    {
+        private int teamid;
+
+        public int TeamId 
+        {
+            get { return teamid; }
+            set { teamid = value; }
+        }
+
+        public TeamLeader (string name, int id, int teamId) 
+        {
+            this.name = name;
+            this.id = id;
+            this.teamid = teamId;
+        }
+    }
+}
